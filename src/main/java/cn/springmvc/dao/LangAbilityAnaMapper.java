b124b6f76094e32a7596c7f9d54ffd31b5aedbd7
whynot-s <whynot_s@outlook.com>
2018-07-10 09:20:00 +0800

    modify select

--- src/main/java/cn/springmvc/dao/LangAbilityAnaMapper.java
+++ src/main/java/cn/springmvc/dao/LangAbilityAnaMapper.java
@@ -1,5 +1,7 @@
 package cn.springmvc.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -8,7 +10,7 @@ public interface LangAbilityAnaMapper {
 
     ArrayList<String> selectLang();
 
-    ArrayList<Map<String, Object>> selectLangAbility();
+    ArrayList<Map<String, Object>> selectLangAbility(@Param("off") int offset, @Param("lim") int limit);
 
     void saveLangAnalysis(List<Map<String, Object>> output);
 }
--- src/main/java/cn/springmvc/service/LangAbilityAna.java
+++ src/main/java/cn/springmvc/service/LangAbilityAna.java
@@ -28,21 +28,24 @@ public class LangAbilityAna {
             tmp.put(mapKeys[4], -1.0);
             output.put(lang, tmp);
         }
-        ArrayList<Map<String, Object>> langAbilities = langAbilityAnaMapper.selectLangAbility();
         System.out.println("Finish Selecting");
-        System.out.printf("Size : %d\n", langAbilities.size());
-        for(Map<String, Object> langAbility : langAbilities){
-            for(String lang : languages){
-                double abilty = (Double)(langAbility.get(lang));
-                if(Double.compare(abilty, 0.0) == 0){
-                    Map<String, Object> tmp = output.get(lang);
-                    tmp.put(mapKeys[1], ((Integer)(tmp.get(mapKeys[1]))) + 1);
-                    tmp.put(mapKeys[2], ((Double)(tmp.get(mapKeys[2]))) + abilty);
-                    double min = (Double)(tmp.get(mapKeys[3]));
-                    double max = (Double)(tmp.get(mapKeys[4]));
-                    if(abilty < min) tmp.put(mapKeys[3], abilty);
-                    if(abilty > max) tmp.put(mapKeys[4], abilty);
-                    output.put(lang, tmp);
+        for(int offset = 0, limit = 10000; ; offset += limit) {
+            ArrayList<Map<String, Object>> langAbilities = langAbilityAnaMapper.selectLangAbility(offset, limit);
+            System.out.printf("offset:%d limit:%d size:%d\n", offset, limit, langAbilities.size());
+            if(langAbilities.size() == 0) break;
+            for (Map<String, Object> langAbility : langAbilities) {
+                for (String lang : languages) {
+                    double abilty = (Double) (langAbility.get(lang));
+                    if (Double.compare(abilty, 0.0) == 0) {
+                        Map<String, Object> tmp = output.get(lang);
+                        tmp.put(mapKeys[1], ((Integer) (tmp.get(mapKeys[1]))) + 1);
+                        tmp.put(mapKeys[2], ((Double) (tmp.get(mapKeys[2]))) + abilty);
+                        double min = (Double) (tmp.get(mapKeys[3]));
+                        double max = (Double) (tmp.get(mapKeys[4]));
+                        if (abilty < min) tmp.put(mapKeys[3], abilty);
+                        if (abilty > max) tmp.put(mapKeys[4], abilty);
+                        output.put(lang, tmp);
+                    }
                 }
             }
         }

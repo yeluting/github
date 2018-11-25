08a1e0210517334540ac6f3fc9e2fcec2a69ac86
whynot-s <whynot_s@outlook.com>
2018-11-25 08:10:09 +0800

    change test

--- src/test/java/cn/springmvc/test/AppTest.java
+++ src/test/java/cn/springmvc/test/AppTest.java
@@ -21,22 +21,9 @@ public class AppTest
 
     @Test
     public void test() {
-        recommendGithub.setSetID(1);
-        for(int disId = 5; disId <= 8; disId++){
-            recommendGithub.setDisID(disId);
-            recommendGithub.loadExpDataSet();
-            for(int recType = 0; recType < 4; recType++) {
-                recommendGithub.setRecType(recType);
-                int[] tmp = new int[recommendGithub.memberNeeded.length];
-                for (int i = 0; i < tmp.length; i++) tmp[i] = recommendGithub.memberNeeded[i];
-                int i = 0;
-                for (int userId : recommendGithub.users) {
-                    recommendGithub.recommend(userId, "github", recommendGithub.memberNeeded, recommendGithub.skills);
-                    System.out.printf("SET%d DIS%d RecType%d : %d/%d\n", 1, disId, recType, ++i, recommendGithub.users.size());
-                    for (int j = 0; j < tmp.length; j++) recommendGithub.memberNeeded[j] = tmp[j];
-                }
-            }
-        }
+        int[] memberNeeded = {1,4,1,4};
+        String[] skills = {"css", "javascript", "html", "ruby"};
+        System.out.println(recommendGithub.recommend(2, "github", memberNeeded, skills));
     }
 
 }
